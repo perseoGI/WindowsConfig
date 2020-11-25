0bcf58266d1c7d8f542cb2487da9234f912557b7
petacreepers23 <javiermiguel4b@gmail.com>
2020-11-25 23:08:30 +0100

    Set desktop as relative path
    Set desktop as relative path not absolute as before

--- profile.ps1
+++ profile.ps1
@@ -1,23 +1,23 @@
 #Bienvenida...
 $saludos = @(
-    "Que los dioses de la eficiencia te acompañen...",
-    "Recuerda tomar café antes de programar",
-    "Despues de programar Java, dúchate 2 veces",
+    "Que los dioses de la eficiencia te acompaÃ±en...",
+    "Recuerda tomar cafÃ© antes de programar",
+    "Despues de programar Java, dÃºchate 2 veces",
     "Si funciona no lo toques",
     "CSS aka Counter Strike: Source",
     "No solo minecraft y terraria pueden tener frases guays!",
     "/bin /bam /bin bom ban",
     "El prefijo de una variable global debe ser //",
-    "Si el café de la mañana no te despierta, ¡formatea GLADOS!",
-    "Ordenadores: SI,NO | Ordenadores cuánticos: PUEDE",
-    "En esta sesion, cada vez que no compile, ahorra 1€",
+    "Si el cafÃ© de la maÃ±ana no te despierta, Â¡formatea GLADOS!",
+    "Ordenadores: SI,NO | Ordenadores cuÃ¡nticos: PUEDE",
+    "En esta sesion, cada vez que no compile, ahorra 1â‚¬",
     "for mama.. :( for papa... :( ... FOR INT I = 0; I<5; I++",
     "2h de buscar en google puede ahorrarte 5 minutos de consulta de documentacion",
-    "¿420 es grande? Depende. Errores? Si, Warnings? No.",
+    "Â¿420 es grande? Depende. Errores? Si, Warnings? No.",
     "SELECT * FROM TAZA_CAFE",
     "Venga, haz el 'ls', lo estoy esperando...",
     "C# es en realidad C++++",
-    "Si el código no funciona, vuelve a ejecutarlo por si acaso",
+    "Si el cÃ³digo no funciona, vuelve a ejecutarlo por si acaso",
     "Si te sientes mal, recuerda que hay gente que conecta el frontend directamente con la base de datos xd",
     "Si te sientes mal, recuerda que hay gente que quiere instalar unix en vez de linux",
     "Si te sientes mal, recuerda que hay gente que duda si se puede hacer un bucle for en eclipse",
@@ -27,28 +27,28 @@ $saludos = @(
     "ESC :q!"
     "Segmentation fault (core dumped)",
     "0 Errors, 6673 Warnings... :)",
-    "Dale un programa a un hombre, y le frustrarás un día. Enséñale a programar y le frustrarás toda la vida.",
+    "Dale un programa a un hombre, y le frustrarÃ¡s un dÃ­a. EnsÃ©Ã±ale a programar y le frustrarÃ¡s toda la vida.",
     "if(coffee.empty){coffee.refill()}else{coffee.drink()}",
-    "El otro día había tan poca contaminación que podían verse los datos en la nube",
+    "El otro dÃ­a habÃ­a tan poca contaminaciÃ³n que podÃ­an verse los datos en la nube",
     "std::cout << ""hola mundo!"";",
-    "Usa linux!, no por nada, pero así parecerás más hacker",
-    "La nataciâ€ºn es un gran deporte!",
-    "Aquella frase bugueada sobre la natación, no era un bug xd",
-    "Cuando alguien haga código espagueti, enviale una foto de salsa de tomate."
+    "Usa linux!, no por nada, pero asÃ­ parecerÃ¡s mÃ¡s hacker",
+    "La nataciÃ¢â‚¬Âºn es un gran deporte!",
+    "Aquella frase bugueada sobre la nataciÃ³n, no era un bug xd",
+    "Cuando alguien haga cÃ³digo espagueti, enviale una foto de salsa de tomate."
     "CTLR+zzzzz CTLR+c CTLR+yyyyy CTLR+v PARKOUR!",
     "CPU has Halted.",
     "500 Internal Server Error",
     "410 Gone.",
     "Errors are red, my screen is blue. Some one help me! I've deleted ""Sys32""",
     "Roses are red, violets are blue. Unexpected '}' on line 32."
-    "Esta es la frase 42. Que tengas un buen día..."
+    "Esta es la frase 42. Que tengas un buen dÃ­a..."
 )
 $saludos | Get-Random
 Write-Host ""
 
 #Variables de entorno
 Remove-Variable -Force HOME
-Set-Variable HOME "C:\Users\usuario\desktop"
+Set-Variable HOME [Environment]::GetFolderPath("Desktop")
 cd $HOME
 
 #Alias
